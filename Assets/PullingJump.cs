544f57fd197df2967537242d9a3f3584e8dd7ed8
今村エリカ <k023g1045@g.neec.ac.jp>
2024-08-06 02:09:44 +0900

    衝突判定

--- Assets/PullingJump.cs
+++ Assets/PullingJump.cs
@@ -10,6 +10,35 @@ public class PullingJump : MonoBehaviour
     [SerializeField]
     private float jumpPower = 10;
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("衝突した");
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        // 衝突している点の情報が複数格納されている
+        ContactPoint[] contacts = collision.contacts;
+        // 0番目の衝突情報から、衝突している点の情報を取得
+        Vector3 otherNormal = contacts[0].normal;
+        // 上方向を示すベクトル
+        Vector3 upVector = new Vector3(0, 1, 0);
+        // 上方向と法線の内積
+        float dotUN = Vector3.Dot(upVector, otherNormal);
+        // 内積値に逆三角関数arccosをかけて角度を算出し、度数法へ変換
+        float dotDeg = Mathf.Acos(dotUN) * Mathf.Rad2Deg;
+        // 二つのベクトルがなす角度が45度より小さければ再びジャンプ可能
+        if (dotDeg <= 45)
+        {
+            isCanJump = true;
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        isCanJump = false;
+    }
+
+    private bool isCanJump; // ジャンプできるか
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,7 +54,7 @@ public class PullingJump : MonoBehaviour
         if (Input.GetMouseButtonDown(0)) {
             clickPosition = Input.mousePosition;
         }
-        if (Input.GetMouseButtonUp(0)) { 
+        if (isCanJump && Input.GetMouseButtonUp(0)) { 
             // クリックした座標と離した座標の差分を取得
             Vector3 dist = clickPosition - Input.mousePosition;
             // クリックとリリースが同じ座標ならば無視
